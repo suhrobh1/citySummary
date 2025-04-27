8ec19164501ca22d6ba184bbc1368aef8ea0304b
suhrobh1 <93023422+suhrobh1@users.noreply.github.com>
2025-04-27 16:46:11 -0700

    fix

--- server.js
+++ server.js
@@ -1,36 +1,3 @@
-// import OpenAI from 'openai';
-// import dotenv from  'dotenv';
-
-// dotenv.config();
-
-// const key = process.env.apiKey
-
-// const openai = new OpenAI({
-//   apiKey: key,
-//   baseURL: 'https://integrate.api.nvidia.com/v1',
-// })
-
-// async function main() {
-//   const completion = await openai.chat.completions.create({
-//     model: "nvidia/llama-3.3-nemotron-super-49b-v1",
-//     messages: [{"role":"system","content":"couple of sentences on Berlin"}],
-//     temperature: 0.6,
-//     top_p: 0.95,
-//     max_tokens: 4096,
-//     frequency_penalty: 0,
-//     presence_penalty: 0,
-//     stream: true,
-//   })
-   
-//   for await (const chunk of completion) {
-//     process.stdout.write(chunk.choices[0]?.delta?.content || '')
-//   }
-  
-// }
-
-// main();
-
-
   import OpenAI from 'openai';
   import dotenv from  'dotenv';
   import express from 'express';
